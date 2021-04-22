4d44c67638d147941f62c2d7032f78985f8436bf
pouple <antonin.verdier@laposte.net>
2021-04-22 02:37:20 +0200

    optimization"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pickle as pkl
 import cmath
 import keras
 import librosa
@@ -15,37 +16,21 @@ from keras.layers import Input, Dense, InputLayer, Flatten, Reshape
 #import keras
 
 # Load nsynth audio data randomly
-def load_data(folder):
+def load_data(folder, cap=None):
+	magnitudes, phases = [], []
 	files = os.listdir(folder)
-	files = np.random.choice(files, 30000)
-
-	sounds = {}
+	if cap:
+		files = np.random.choice(files, cap)
 
 	for i, file in tqdm(enumerate(files), total=len(files)):
-		sample, samplerate = librosa.load(os.path.join(folder, file),
-								  sr=None, mono=True, offset=0.0, duration=None)
-		sounds['{}'.format(file[:-4])] = sample
+		sample, samplerate = librosa.load(os.path.join(folder, file), sr=None)
 
-	return sounds, samplerate
+		f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
 
-# Perform STFT
-# Actual shallow AE, LSTM-AE,
+		magnitudes.append(np.abs(Zxx))
+		phases.append(np.angle(Zxx))
 
-# Testing and extraction of latent dimension
-
-
-def get_mag_phases(sounds):
-	""" Could be better to use map function and apply the function along the list
-	"""
-	magnitudes, phases = [], []
-	for i, k in tqdm(enumerate(sounds), total=len(sounds)):
-		f, t, Zxx = signal.stft(sounds[k], fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
-		mag = np.abs(Zxx)
-		p = np.angle(Zxx)
-		magnitudes.append(mag)
-		phases.append(p)
-
-	return np.array(magnitudes), np.array(phases)
+	return np.array(magnitudes), np.array(phases), samplerate
 
 
 def build_autoencoder(sound_shape, latent_dim):
@@ -66,30 +51,27 @@ def build_autoencoder(sound_shape, latent_dim):
 
 	return encoder, decoder
 
-# sounds, samplerate = load_data('/home/pouple/PhD/Code/Dimmy/Data/nsynth-train/audio')
-# mag, phases = get_mag_phases(sounds)
+mag, phases, samplerate = load_data('/home/pouple/PhD/Code/Dimmy/Data/nsynth-train/audio', cap=30000)
 
-# encoder, decoder = build_autoencoder(mag.shape[1:], 20)
+encoder, decoder = build_autoencoder(mag.shape[1:], 40)
 
-# inp = Input(mag.shape[1:])
-# code = encoder(inp)
-# reconstruction = decoder(code)
+inp = Input(mag.shape[1:])
+code = encoder(inp)
+reconstruction = decoder(code)
 
-# autoencoder = Model(inp, reconstruction)
-# autoencoder.compile(optimizer='adam', loss='mse')
+autoencoder = Model(inp, reconstruction)
+autoencoder.compile(optimizer='adam', loss='mse')
 
 
-# history = autoencoder.fit(mag, mag, epochs=100)
-# autoencoder.save('Autoencoder_model')
+history = autoencoder.fit(mag, mag, epochs=100)
+autoencoder.save('Autoencoder_model')
+
+pickle.dump(open('model_history.pkl', 'w'), history)
+
 autoencoder = keras.models.load_model('Autoencoder_model')
 
-sounds_test, samplerate = load_data('/home/pouple/PhD/Code/Dimmy/Data/nsynth-valid/audio')
-mag_t, phases_t = get_mag_phases(sounds_test)
-
-
-
-decoded_mag = autoencoder.evaluate(mag_t, mag_t)
-print(decoded_mag)
+mag_t, phases_t, samplerate = load_data('/home/pouple/PhD/Code/Dimmy/Data/nsynth-valid/audio')
+decoded_mag = autoencoder.predict(mag_t)
 
 Zxx = decoded_mag * np.exp(phases_t*1j)
 
