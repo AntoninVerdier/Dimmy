5ee5b93199956c2c5ab96933d4c0022e84a36fac
pouple <antonin.verdier@laposte.net>
2021-05-11 14:32:49 +0200

    reconstruct function

--- main.py
+++ main.py
@@ -57,54 +57,73 @@ def build_autoencoder(sound_shape, latent_dim):
 	return encoder, decoder
 
 
+def reconstruct_sound(latent):
+	decoded_mag = decoder.predict(latent)
+	k = decoded_mag * np.exp(0j)
+	t, sound = signal.istft(k, fs=16000, window='hamming', nperseg=1024, noverlap=512)
+	print(sound)
+	return sound
+
 # specs, files, samplerate = load_data('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-train/audio')
 partition = {}
 params = {'dim': (513,126),
           'batch_size': 256,
-          'shuffle': True,}
+          'shuffle': False,}
 
-partition['train'] = os.listdir('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-train/audio')
-partition['validation'] = os.listdir('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-valid/audio')
+# partition['train'] = os.listdir('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-train/audio')
+# partition['validation'] = os.listdir('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-valid/audio')
+partition['test'] = os.listdir('/home/user/Documents/Antonin/Code/Dimmy/Data/nsynth-test/audio')
 
-training_generator = DataGenerator(partition['train'], **params)
-validation_generator = DataGenerator(partition['validation'], **params)
+# training_generator = DataGenerator(partition['train'], **params)
+# validation_generator = DataGenerator(partition['validation'], **params)
 
-pkl.dump(training_generator.indexes, open('Output/train_indexes.pkl', 'wb'))
-pkl.dump(validation_generator.indexes, open('Output/valid_indexes.pkl', 'wb'))
+# pkl.dump(training_generator.indexes, open('Output/train_indexes.pkl', 'wb'))
+# pkl.dump(validation_generator.indexes, open('Output/valid_indexes.pkl', 'wb'))
 
 
-encoder, decoder = build_autoencoder((513, 126), 25)
+# encoder, decoder = build_autoencoder((513, 126), 25)
 
-inp = Input((513, 126))
-code = encoder(inp)
-reconstruction = decoder(code)
+# inp = Input((513, 126))
+# code = encoder(inp)
+# reconstruction = decoder(code)
 
-autoencoder = Model(inp, reconstruction)
-autoencoder.compile(optimizer='adam', loss='mse')
+# autoencoder = Model(inp, reconstruction)
+# autoencoder.compile(optimizer='adam', loss='mse')
 
-history = autoencoder.fit(training_generator,
-						  validation_data=validation_generator,
-						  epochs=120)
+# history = autoencoder.fit(training_generator,
+# 						  validation_data=validation_generator,
+# 						  epochs=120)
 
-autoencoder.save('Autoencoder_model')
-encoder.save('Encoder_model')
-decoder.save('Decoder_model')
+# autoencoder.save('Autoencoder_model')
+# encoder.save('Encoder_model')
+# decoder.save('Decoder_model')
 
-pkl.dump(history.history, open('Output/model_history.pkl', 'wb'))
+# pkl.dump(history.history, open('Output/model_history.pkl', 'wb'))
 
-# autoencoder = load_model('Autoencoder_model')
-# encoder = load_model('Encoder_model')
-# decoder = load_model('Decoder_model')
+autoencoder = load_model('Autoencoder_model')
+encoder = load_model('Encoder_model')
+decoder = load_model('Decoder_model')
+
+
+# for i in range(25):
+# 	test = np.zeros(shape=(1, 25))
+# 	test[0, i] = 1
+# 	sound = reconstruct_sound(test)
+# 	wavfile.write('Sounds_unique/Sound_{}'.format(i), 16000, sound[0])
+
 
 
 test_generator = DataGenerator(partition['test'], test=True, **params)
+print(test_generator.list_IDs)
 
 prediction = encoder.predict(test_generator)
 
 fig, axs = plt.subplots(10, 10, figsize=(20, 20))
 
-for i in range(100):
-	axs[i//10, i%10].imshow(prediction[i].reshape(5, 5))
+kb_elec = [i for i, f in enumerate(test_generator.list_IDs) if 'keyboard_electronic' in f]
+
+for i, j in enumerate(kb_elec[:100]):
+	axs[i//10, i%10].imshow(prediction[j].reshape(5, 5))
 
 plt.show()
 
@@ -121,8 +140,10 @@ plt.show()
 decoded_mag = autoencoder.predict(test_generator)
 phases = DataGenerator(partition['test'], test=True, phase=True, **params)
 phases = np.array([p for p in phases]).reshape(4096, 513, 126)
+plt.imshow(phases[0])
+plt.show()
 
-Zxx = decoded_mag * np.exp(phases*1j)
+Zxx = decoded_mag * np.exp(0j)
 
 reconstructed_sounds = []
 for i, k in enumerate(Zxx):
