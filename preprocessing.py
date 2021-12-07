1ca9fcedc740e0d04955eca80819d949f5fa9fe1
pouple <antonin.verdier@laposte.net>
2021-12-07 10:16:18 +0100

    clean data prepro

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import pywt
 import pickle as pkl
 import librosa
 import numpy as np
+from rich.progress import track
 
 from tqdm import tqdm
 
@@ -19,110 +20,28 @@ from scipy.spatial.distance import cosine
 
 # Goal is to clarify this file and make a large function that can generate the dataset with the preprocessing steps required (mel,log-scaled, etc.)
 
-
-def load_data(folder, cap=None):
+def load_data_array(folder):
 	files = os.listdir(folder)
 
 	ids = [f[:-4] for f in files]
 
-	dataset = {}
+	dataset = np.empty((len(ids), 513, 126), dtype=np.float16)
 
-	for i, file in enumerate(tqdm(ids)):
+	for i, file in enumerate(track(ids)):
 		sample, samplerate = librosa.load(os.path.join(folder, file + '.wav'), sr=16000)
+
 
 		f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
 
 		mag = np.abs(Zxx)
-		dataset[file] = mag
 
-	all_max = 0
-	for k in dataset:
-		curr_max = np.max(dataset[k])
-		if curr_max > all_max:
-			all_max = curr_max
-
-	for k in dataset:
-		dataset[k] = np.array((dataset[k]/all_max)*(2**16), dtype=np.uint16)
-
-	pkl.dump(dataset, open('dataset1.pkl', 'wb'))
-
-def load_file(file):
-	sample, samplerate = librosa.load(file, sr=192000)
-	sample = librosa.resample(sample, 192000, 16000)
-	samplerate = 16e3
-
-	f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
+		dataset[i, :, :] = np.array(((mag - np.min(mag))/np.max(mag))*255, dtype=np.uint8)
 
 
-	mag = np.log(1 + np.abs(Zxx)).astype(np.float16)
-	mag = (mag / np.max(mag))*255
-	mag = np.array(mag, dtype=np.uint8)
-
-	return mag
-
-def load_data_array(folder, cap=None):
-	files = os.listdir(folder)
-
-	ids = [f[:-4] for f in files]
-
-	dataset = np.empty((len(ids), 513, 189), dtype=np.float16)
-
-	for i, file in enumerate(tqdm(ids)):
-		sample, samplerate = librosa.load(os.path.join(folder, file + '.wav'), sr=16000)
+	pkl.dump(dataset, open('dataset_train.pkl', 'wb'))
 
 
-		f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
-		print(np.abs(Zxx).shape)
-
-		mag = np.log(1 + np.abs(Zxx))
-
-		plt.imshow(mag)
-		plt.show()
-
-		dataset[i, :, :] = mag.astype(np.float16)
-		#dataset[i, :, :] = np.angle(Zxx).astype(np.float16)
-
-
-	print(np.max(dataset), np.min(dataset))
-	dataset = np.array(dataset/np.max(dataset), dtype=np.float16)
-	dataset = dataset * 255
-	dataset = np.array(dataset, dtype=np.uint8)
-
-	print(np.max(dataset), np.min(dataset))
-
-	pkl.dump(dataset, open('dataset_test_pupcages.pkl', 'wb'))
-
-
-		
-
-
-#load_data_array('/home/pouple/PhD/Data/splitted')
-
-def test_load_data_array(folder, cap=None):
-	files = os.listdir(folder)
-
-	ids = [f[:-4] for f in files]
-
-	dataset = np.empty((len(ids), 513, 126), dtype=np.float64)
-
-	for i, file in enumerate(tqdm(ids)):
-		sample, samplerate = librosa.load(os.path.join(folder, file + '.wav'), sr=16000)
-
-		f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
-
-		#mag = np.log(1 + np.abs(Zxx))
-		mag = np.abs(Zxx)
-		
-
-		#mag = np.exp(mag) - 1
-
-		Zxx = mag * np.angle(Zxx)*1j
-
-		#dataset[i, :, :] = np.angle(Zxx).astype(np.float16)
-	  	
-		t, sound = signal.istft(Zxx, fs=16000, window='hamming', nperseg=1024, noverlap=512)
-		wavfile.write('Sounds_testing/Sound_36_256_{}'.format(ids[i]), 16000, sound)
-
+load_data_array('/home/pouple/PhD/Code/Dimmy/Data/nsynth-train/audio')
 	
 
 	# print(np.max(dataset), np.min(dataset))
@@ -134,7 +53,6 @@ def test_load_data_array(folder, cap=None):
 
 	# pkl.dump(dataset, open('dataset_test.pkl', 'wb'))
 
-
 def load_data_multi(file):
 
 	sample, samplerate = librosa.load(os.path.join(folder, file + '.wav'), sr=16000)
@@ -144,23 +62,6 @@ def load_data_multi(file):
 	mag = np.log(1 + np.abs(Zxx))
 
 	dataset[i, :, :] = mag
-
-
-def load_data_to_wl(folder):
-	files = os.listdir(folder)
-
-	ids = [f[:-4] for f in files]
-
-	dataset = np.empty((len(ids), 513, 126), dtype=np.float64)
-
-	for i, file in enumerate(tqdm(ids)):
-		sample, samplerate = librosa.load(os.path.join(folder, file + '.wav'), sr=16000)
-		coef, freqs=pywt.cwt(sample ,np.arange(1,129),'mexh')
-		fig = plt.figure()
-		ax = fig.add_subplot(111)
-		ax.matshow(coef) # doctest: +SKIP
-		plt.axis('auto')
-		plt.show()
 
 def correlation_matrix(projections):
 	""" Projections must be of the form : (n_proj, x, y)"""
@@ -176,21 +77,6 @@ def corrleation(arr, brr):
 
 def cosine_distance(arr, brr):
 	return cosine(arr.flatten(), brr.flatten())
-
-
-
-def convert_to_dlp(projection):
-	""" DLP has a resolution of 480 by 300px"""
-	projection = projection.reshape(10, 10)
-	fig, ax = plt.subplots(1, 2)
-	ax[0].imshow(projection)
-
-
-	projection = resize(projection, (300, 300), anti_aliasing=False)
-	projection = np.array([90*[0] + list(p) + 90*[0] for p in projection]).reshape(300, 480)
-	ax[1].imshow(projection, )
-	plt.show()
-	plt.close()
 
 
 
