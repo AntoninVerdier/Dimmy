847bbefadb2453038ecd3537eb0df9f16cb5c2eb
pouple <antonin.verdier@laposte.net>
2021-12-14 12:27:11 +0100

    add multiprocess support for spec gen

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 from scipy import signal
 
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 
 from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
@@ -51,7 +51,7 @@ def load_file(file, mod=None):
 	#samplerate = 16e3
 
 	f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
-	
+
 	if mod == 'log':
 		mag = np.log(1 + np.abs(Zxx))
 	else:
@@ -60,6 +60,37 @@ def load_file(file, mod=None):
 	mag = np.array(((mag - np.min(mag))/np.max(mag))*255, dtype=np.uint8)
 
 	return mag
+
+def load_unique_file(arg, mod=None):
+	path, mod = arg
+	sample, samplerate = librosa.load(os.path.join(path), sr=400000)
+	sample = librosa.resample(sample, samplerate, 192000)
+	samplerate = 192e3
+
+	f, t, Zxx = signal.stft(sample, fs=samplerate, window='hamming', nperseg=1024, noverlap=512)
+
+	if mod == 'log':
+		mag = np.log(1 + np.abs(Zxx))
+	else:
+		mag = np.abs(Zxx)
+
+	#dataset[i, :, :] = np.array(((mag - np.min(mag))/np.max(mag))*255, dtype=np.uint8)
+	return np.array((mag - np.min(mag))/np.max(mag)*255, dtype=np.uint8)
+
+def load_data_array_multi(folder, filename='dataset', mod=None):
+	files = os.listdir(folder)
+	ids = [f[:-4] for f in files]
+
+	dataset = []
+
+	paths = [(os.path.join(folder, file), mod) for file in files]
+
+	with Pool() as p:
+		results = [p.apply_async(load_unique_file, args=(path, )) for path in paths]
+		dataset = [p.get() for p in track(results, description='Computing spectrograms ...')]
+
+	dataset = np.array(dataset)
+	pkl.dump(dataset, open(filename, 'wb'))
 
 def correlation_matrix(projections):
 	""" Projections must be of the form : (n_proj, x, y)"""
@@ -77,7 +108,7 @@ def cosine_distance(arr, brr):
 	return cosine(arr.flatten(), brr.flatten())
 
 if __name__ == '__main__':
-	load_data_array('/home/user/Documents/Antonin/Dimmy/audio_wav', mod='log')
+	load_data_array_multi('/home/pouple/PhD/Code/Dimmy/Data/audio_wav', mod='log', filename='dataset_multi_cnn')
 
 
 		
