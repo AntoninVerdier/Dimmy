f1601aaf97ad08bec8ee7fc8f32cf35a13cf025f
Antonin Verdier <antonin.verdier@laposte.net>
2022-12-20 15:05:33 +0100

    added doctsrings

--- main.py
+++ main.py
@@ -2,35 +2,28 @@ import os
 import json
 import datetime
 import argparse
+import visualkeras
 import numpy as np
 import natsort as n
 import pickle as pkl
-import keras_tuner
-from sklearn import preprocessing as p
+import settings as s
+import preproc as proc
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from tensorflow.keras.callbacks import TensorBoard
+from PIL import ImageFont
+from Models import Autoencoder
+from rich.progress import track
+from rich import print, traceback
+from sklearn import preprocessing as p
+from tensorflow.keras import mixed_precision
+from tensorflow.keras.callbacks import TensorBoard, CallBack
 from tensorflow.keras.models import load_model, Model
 from sklearn.model_selection import train_test_split
 
-from tensorflow.keras.callbacks import Callback
-
-import visualkeras
-from PIL import ImageFont
-import matplotlib.pyplot as plt
-from rich import print, traceback
 traceback.install()
-from rich.progress import track
-
-import settings as s
-import preproc as proc
-from Models import Autoencoder
-
-import tensorflow as tf
-
-from tensorflow.keras import mixed_precision
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 mixed_precision.set_global_policy('mixed_float16')
-
 
 # Define arguments for inline parsing
 paths = s.paths()
@@ -63,7 +56,7 @@ parser.add_argument('--tuner', '-tu', action='store_true',
 args = parser.parse_args()
 
    
-# Execute training if inline argument is passed
+# Execute training if inline argument --train is passed
 if args.train:
     # Get time and date for record when saving
     today = datetime.date.today()
@@ -73,6 +66,7 @@ if args.train:
     today = today.strftime("%d%m%Y")
     time_str = time.strftime("%H%M%S")
 
+    # If inline argument is passed, create callback instance
     if args.callbacks:
       tensorboard = TensorBoard(
         log_dir='./logs',
@@ -80,31 +74,28 @@ if args.train:
         write_images=True,
         update_freq=5
       )
+      keras_callbacks = [tensorboard]
 
-      keras_callbacks = [
-        tensorboard
-      
-      ]
-
-    # Quick infos on the network for record
+    # Allow user to give a name and quick description of the network for future record
     if not args.quicktest:
       net_name = input('Name of the network > ')
       description = input('Small description of the current network training, for record > ')
     else:
       net_name, description = args.quicktest, args.quicktest
 
+#############################################################################
+    # Create folder for logs 
     logs = "new_logs/" + '{}_{}_{}'.format(today, time_str, net_name)
     tboard_callback = tf.keras.callbacks.TensorBoard(log_dir = logs,
                                                      histogram_freq =1,
                                                      profile_batch =(2, 5))
     
+#############################################################################  
     # Datasets
     input_dataset_file = 'heardat_noise_datasetv2_60_cqt_128_28k.pkl'
     output_dataset_file = 'heardat_clean_datasetv2_60_cqt_128_28k.pkl'
     toeplitz_true = 'ordered_96_28k_toeplitz.npy'
     toeplitz_spec = 'topelitz_gaussian_cxe_28k.npy'
-
-    
 
     # Distinguish between noisy input and clean reconstruction target
     X_train = np.load(open(input_dataset_file, 'rb'), allow_pickle=True).astype('float32')/255.0
@@ -116,20 +107,12 @@ if args.train:
     X_train_c = X_train_c[shuffle_mask]
     
     # This to enable fair splitting for convolution. Configured for spectrogram training
-    if args.network: 
+    input_shape = (X_train.shape[1] - X_train.shape[1]%16, X_train.shape[2] - X_train.shape[2]%16)
+    X_train = X_train[:, :input_shape[0], :input_shape[1]]
+    X_train_c = X_train_c[:, :input_shape[0], :input_shape[1]]
 
-      input_shape = (X_train.shape[1] - X_train.shape[1]%16, X_train.shape[2] - X_train.shape[2]%16)
-      print(input_shape)
-      X_train = X_train[:, :input_shape[0], :input_shape[1]]
-      X_train_c = X_train_c[:, :input_shape[0], :input_shape[1]]
-
-
-    # Create a validation set486559206
+    # Create a validation set
     X_train, X_valid, X_train_c, X_valid_c = train_test_split(X_train, X_train_c, test_size=0.2, shuffle=True)
-
-    # train_ds = tf.data.Dataset.from_tensor_slices((X_train, X_train_c)).batch(args.batch_size)
-    # valid_ds = tf.data.Dataset.from_tensor_slices((X_valid, X_valid_c)).batch(args.batch_size)
-
 
     # Create network class
     auto = Autoencoder('{net}'.format(net=args.network if args.network else 'dense'), input_shape, params.latent_size, max_n=args.max_n, toeplitz_true=toeplitz_true, toeplitz_spec=toeplitz_spec)
@@ -153,38 +136,92 @@ if args.train:
       os.makedirs(os.path.join(save_model_path, 'Performances', 'Img'))
       os.makedirs(os.path.join(save_model_path, 'Performances', 'Data'))
 
-
-
     # Launch training with callbacks to tensorboard if specified in inline command
-
     class ToeplitzLogger(Callback):
+      """ Logger function called at each epoch end to generate a correlation matrix to see evolution 
+
+      Attributes
+      ----------
+      test_freq : array
+        Array of spectrograms of pure tones to be predicted
+      
+      Methods
+      -------
+      on_epoch_end
+        Generate a array and a figure of the autocorrelation of predicted pure tones
+      """      
       def __init__(self):
-        self.test_freq = np.load(os.path.join('toeplitz', toeplitz_true), allow_pickle=True)[:, :input_shape[0], :input_shape[1]]/255.0#continuous was trained without
+        """Constructor
+        
+        Load array of spectrograms of pure tones to be predicted and format and normalize them
+        to the required shape.
+        """
+        self.test_freq = np.load(os.path.join('toeplitz', toeplitz_true), allow_pickle=True)[:, :input_shape[0], :input_shape[1]]/255.0
         
 
-      def on_epoch_end(self, epoch, logs=None):
-          pred_freq_corr = autoencoder(self.test_freq)[1]
+      def on_epoch_end(self, epoch):
+        """ Method called at the end of the epoch. 
 
-          def t(a): return tf.transpose(a)
+        Save both an array as a .npy file and a heatmap of the array as .svg. The
+        array is an autocorrelation matrix of the latent representation of pure tone 
+        predicted by the still-training autoencoder.
 
-          x = pred_freq_corr
-          mean_t = tf.reduce_mean(x, axis=1, keepdims=True)
-          #cov_t = x @ t(x)
-          cov_t = ((x-mean_t) @ t(x-mean_t))/(pred_freq_corr.shape[1]-1)
-          cov2_t = tf.linalg.diag(1/tf.sqrt(tf.linalg.diag_part(cov_t)))
-          cor = cov2_t @ cov_t @ cov2_t
+        Parameters
+        ----------
+        epoch : int
+          Current epoch number
+        """
+        pred_freq_corr = autoencoder(self.test_freq)[1]
 
-          np.save(os.path.join(save_model_path, 'Callbacks', 'Dat', 'log_top_{}.npy'.format(epoch)), cor)
-          plt.imshow(cor, vmin=0, vmax=1)
-          plt.savefig(os.path.join(save_model_path, 'Callbacks', 'Img','log_top_{}.svg'.format(epoch)))
-          plt.close()
+        def t(a): return tf.transpose(a)
+
+        x = pred_freq_corr
+        mean_t = tf.reduce_mean(x, axis=1, keepdims=True)
+        #cov_t = x @ t(x)
+        cov_t = ((x-mean_t) @ t(x-mean_t))/(pred_freq_corr.shape[1]-1)
+        cov2_t = tf.linalg.diag(1/tf.sqrt(tf.linalg.diag_part(cov_t)))
+        cor = cov2_t @ cov_t @ cov2_t
+
+        np.save(os.path.join(save_model_path, 'Callbacks', 'Dat', 'log_top_{}.npy'.format(epoch)), cor)
+        plt.imshow(cor, vmin=0, vmax=1)
+        plt.savefig(os.path.join(save_model_path, 'Callbacks', 'Img','log_top_{}.svg'.format(epoch)))
+        plt.close()
     
     class ToeplitzLogger_am(Callback):
+      """ Logger function called at each epoch end to generate a correlation matrix to see evolution 
+
+      Attributes
+      ----------
+      test_freq : array
+        Array of spectrograms of amplitude modulated tones to be predicted
+      
+      Methods
+      -------
+      on_epoch_end
+        Generate a array and a figure of the autocorrelation of predicted amplitude modulated tones
+      """      
+ 
       def __init__(self):
-        self.test_freq_am = np.load(os.path.join('toeplitz', 'toep_am_10k.npy'), allow_pickle=True)[:, :input_shape[0], :input_shape[1]]/255.0#continuous was trained without
+        """Constructor
+        
+        Load array of spectrograms of amplitude-modulated tones to be predicted and format and normalize them
+        to the required shape.
+        """
+        self.test_freq_am = np.load(os.path.join('toeplitz', 'toep_am_10k.npy'), allow_pickle=True)[:, :input_shape[0], :input_shape[1]]/255.0
         
 
       def on_epoch_end(self, epoch, logs=None):
+        """ Method called at the end of the epoch. 
+
+        Save both an array as a .npy file and a heatmap of the array as .svg. The
+        array is an autocorrelation matrix of the latent representation of amplitude-modulated tone 
+        predicted by the still-training autoencoder.
+
+        Parameters
+        ----------
+        epoch : int
+          Current epoch number
+        """
           pred_freq_corr = autoencoder(self.test_freq_am)[2]
 
           def t(a): return tf.transpose(a)
@@ -200,45 +237,8 @@ if args.train:
           plt.imshow(cor, vmin=0, vmax=1)
           plt.savefig(os.path.join(save_model_path, 'Callbacks', 'Img','logam_top_{}.svg'.format(epoch)))
           plt.close()
-    
-    class CorrLogger(Callback):
-      def __init__(self):
-        self.sounds_to_encode = '/home/user/Documents/Antonin/Dimmy/Data/SoundsHearlight'
-        x = np.arange(1, 129)
-        y = 0.5*np.exp(-0.022*x)
-        y = 1/(np.repeat(y, 126).reshape(128, 126))
-        # Load soundfile and compute spectrogram
-        
-        xs = []
-        for i, f in track(enumerate(n.natsorted(os.listdir(self.sounds_to_encode))), total=len(os.listdir(self.sounds_to_encode))):
-          X_test = np.expand_dims(proc.load_unique_file_cqt(os.path.join(self.sounds_to_encode, f), y), 0)
-          
-          X_test = X_test.astype('float32')/255.0
-          X_test = X_test[:, :input_shape[0], :input_shape[1]]
-          X_test = np.expand_dims(X_test, 3)
 
-          xs.append(X_test)
-        
-        
-
-      def on_epoch_end(self, epoch, logs=None):
-          pred_freq_corr = autoencoder(self.test_freq_am)[2]
-
-          def t(a): return tf.transpose(a)
-
-          x = pred_freq_corr
-          mean_t = tf.reduce_mean(x, axis=1, keepdims=True)
-          #cov_t = x @ t(x)
-          cov_t = ((x-mean_t) @ t(x-mean_t))/(pred_freq_corr.shape[1]-1)
-          cov2_t = tf.linalg.diag(1/tf.sqrt(tf.linalg.diag_part(cov_t)))
-          cor = cov2_t @ cov_t @ cov2_t
-
-          np.save(os.path.join(save_model_path, 'Callbacks', 'Dat', 'logam_top_{}.npy'.format(epoch)), cor)
-          plt.imshow(cor, vmin=0, vmax=1)
-          plt.savefig(os.path.join(save_model_path, 'Callbacks', 'Img','logam_top_{}.svg'.format(epoch)))
-          plt.close()
-
-    print(np.min(X_train), np.max(X_train))
+    # Finally train the model
     history = autoencoder.fit(X_train, X_train_c,
                             validation_data=(X_valid, X_valid_c),
                             epochs=args.epochs, 
@@ -252,13 +252,10 @@ if args.train:
 
     # Add a timestamp to log files and model name so file is unique. 
     # Add ID to load it faster for further exp - WIP
-    
     td = datetime.datetime.now() - time
     training_time = '{}h {}m {}s'.format(td.seconds//3600, (td.seconds//60)%60, td.seconds%60)
 
     autoencoder.save(os.path.join(save_model_path, 'Autoencoder_model_{}_{}'.format(args.network, net_name)))
-    # encoder.save(os.path.join(save_model_path, 'Encoder_model_{}_{}'.format(args.network, net_name)))
-    # decoder.save(os.path.join(save_model_path, 'Decoder_model_{}_{}'.format(args.network, net_name)))
 
     pkl.dump(history.history, open(os.path.join(save_model_path, 'model_history.pkl'), 'wb'))
 
@@ -277,12 +274,11 @@ if args.train:
     args_dict['best_loss'] = np.min(history.history['loss'])
     args_dict['end_loss'] = history.history['loss'][-1]
 
+    # Output keras model summary in txt file
     with open(os.path.join(save_model_path, 'summary.txt'), 'w') as f:
       autoencoder.summary(print_fn=lambda x: f.write(x + '\n'))
 
-
-    # Add training time
-
+    # Save all configurations parameters and training stats in jdon file
     with open(os.path.join(save_model_path, 'metadata.json'), 'w') as f:
       json.dump(args_dict, f, indent=4)
 
@@ -291,21 +287,22 @@ if args.train:
 
     # Loop trough each sound and output the latent representation
     for i, f in track(enumerate(n.natsorted(os.listdir(sounds_to_encode))), total=len(os.listdir(sounds_to_encode))):
-      # To clean
+      # Normalize with exponential function to avoid under-representation of high frequencies
       x = np.arange(1, 129)
       y = 0.5*np.exp(-0.022*x)
       y = 1/(np.repeat(y, 126).reshape(128, 126))
+
       # Load soundfile and compute spectrogram
       X_test = np.expand_dims(proc.load_unique_file_cqt(os.path.join(sounds_to_encode, f), y), 0)
       X_test = X_test.astype('float32')/255.0
       X_test = X_test[:, :input_shape[0], :input_shape[1]]
       X_test = np.expand_dims(X_test, 3)
 
+      # Construct encoder with and without gaussian blurring from entire model
       encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('Dense_maxn').output)
       blurred_output = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('gaussian_blur').output)
 
-      
-      # Get prediction
+      # Get sharp and blurred latent representation 
       latent_repre = encoder(X_test)
       blurred = blurred_output(X_test) 
 
@@ -322,7 +319,6 @@ if args.train:
       plt.close()
 
       final_spec = autoencoder(X_test)[0]
-
 
       # Make figure of comparison side by side
       fig, axs = plt.subplots(1, 2)
@@ -350,8 +346,6 @@ if args.train:
     filenames = n.natsorted(os.listdir(path))
     np.save(os.path.join(save_model_path, 'Performances', 'Data', 'filenames.npy'), filenames)
     all_latent = np.array([np.load(os.path.join(path, s)) for s in n.natsorted(os.listdir(path))]).reshape(len(os.listdir(path)), 100)
-
-
 
     corr_matrix = proc.correlation_matrix(all_latent)
     np.save(os.path.join(save_model_path, 'Performances', 'Data','corr_matrix.npy'), corr_matrix)
@@ -407,10 +401,6 @@ if args.predict:
 
     X_test = np.expand_dims(X_test, 3)
 
-
-    
-
-    
     # Get prediction
     latent_repre = encoder(X_test)
     np.save(os.path.join('Latent', '{}_latent.npy'.format(f[:-4])), latent_repre.reshape(100))
@@ -419,15 +409,7 @@ if args.predict:
     #np.save(os.path.join('Latent', '{}_spec.npy'.format(f[:-4])), final_spec)
     plt.imshow(final_spec.reshape(128, 112))
     plt.close()
-    #plt.show()
 
-
-    # Plot latent representation as an intensity pattern
-    # plt.imshow(p.normalize(latent_repre.reshape(10, 10)), cmap='Blues')
-    # plt.colorbar()
-    # plt.savefig('latent_repre_{}.svg'.format(f))
-    # plt.close()
-    
     # Extract blurred representation from early intermediate layer in decoder
     blurred_output = Model(inputs=decoder.input, outputs=decoder.get_layer('gaussian_blur').output)
     blurred = blurred_output(latent_repre)
@@ -446,6 +428,7 @@ if args.predict:
     #plt.show()
     plt.close()
 
+# Enter visualisation routine if specified in the inline command
 if args.visualize:
   # Use visual keras to have a quick view of the model architecture
   autoencoder = load_model(os.path.join(paths.path2Models,'Autoencoder_model_{}'.format(args.network)))
